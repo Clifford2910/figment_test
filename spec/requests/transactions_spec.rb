76e724681dc73a6fd674b23fb5a75a6a7f28d4cf
Alex Clifford <aclifford336@gmail.com>
2025-03-31 22:26:33 +0100

    refactor tests taking into consideration new api structure

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Transactions", type: :request do
-  describe "GET /index" do
+  describe "GET /" do
+    before do
+      stub_request(:get, %r{near/transactions}).to_return(
+        status: 200,
+        body: [].to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    end
+
     it "returns http success" do
-      get "/transactions/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/services/fetch_transactions_spec.rb
+++ spec/services/fetch_transactions_spec.rb
@@ -7,23 +7,31 @@ RSpec.describe FetchTransactions do
         "hash" => "abc123",
         "block_hash" => "block123",
         "height" => 1,
-        "action" => {
-          "type" => "transfer",
-          "from" => "alice.near",
-          "to" => "bob.near",
-          "deposit" => "1000"
-        }
+        "actions" => [
+          {
+            "type" => "transfer",
+            "data" => {
+              "deposit" => "1000"
+            }
+          }
+        ],
+        "sender" => "alice.near",
+        "receiver" => "bob.near"
       },
       {
         "hash" => "def456",
         "block_hash" => "block456",
         "height" => 2,
-        "action" => {
-          "type" => "stake", # should be ignored
-          "from" => "bob.near",
-          "to" => "staking.pool",
-          "deposit" => "500"
-        }
+        "actions" => [
+          {
+            "type" => "stake",
+            "data" => {
+              "deposit" => "500"
+            }
+          }
+        ],
+        "sender" => "bob.near",
+        "receiver" => "staking.pool"
       }
     ]
   end
@@ -42,6 +50,8 @@ RSpec.describe FetchTransactions do
 
     tx = Transaction.last
     expect(tx.transaction_hash).to eq("abc123")
+    expect(tx.block_hash).to eq("block123")
+    expect(tx.height).to eq(1)
     expect(tx.sender).to eq("alice.near")
     expect(tx.receiver).to eq("bob.near")
     expect(tx.deposit).to eq("1000")
